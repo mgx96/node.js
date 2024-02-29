64afa23b0c0edcfebfff1784a3455a8e75d0234a
Malek Sharabi <m.malek1996@hotmail.com>
2024-02-29 22:49:30 +0100

    NPM

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,10 @@
+/*var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);*/
+
+const superheroes = require('superheroes');
+superheroes.all;
+
+var randomName = superheroes.random();
+console.log(`I am ${randomName}`);
